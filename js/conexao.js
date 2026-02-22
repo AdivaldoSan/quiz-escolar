cdc7733e511d8abd124d81d2ad7825464f655cba
AdivaldoSan <professoradivaldo@gmail.com>
2026-02-22 16:13:20 -0400

    Update conexao.js

--- js/conexao.js
+++ js/conexao.js
@@ -1,5 +1,5 @@
 const API =
-"https://script.google.com/macros/s/AKfycbwoeCI-gCMXauWzpTQUekSsotpPFXnhknsEUjyM8T3T_RxUv2c_v_0Ud8ACCCOazUgqfw/exec";
+"https://script.google.com/macros/s/AKfycbzVkBqTlzyRMIEq2f1ZP6qOeJ9y5lNfmI83qLpCIsCxnhyTf_TXTb4_TDguW-j5x5XwkA/exec;
 
 
 // ===============================
